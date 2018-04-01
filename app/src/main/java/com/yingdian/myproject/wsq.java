7dd222073d8a67a6dd92445bb95bc076f3c054ea
wangshuqiang11 <864814954@qq.com>
2018-04-01 20:11:35 +0800

    fdsfdsf

---
+++ app/src/main/java/com/yingdian/myproject/wsq.java
@@ -0,0 +1,28 @@
+package com.yingdian.myproject;
+
+import android.util.Log;
+
+/**
+ * Created by WSQ on 2018/4/1 0001.
+ */
+
+public class wsq {
+
+    //修改上传
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "wsq{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
